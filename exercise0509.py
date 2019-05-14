7574c846ac3324c82d52b68231c1815c9bd669be
Kota Kawakami <is0302ke@gmail.com>
2019-05-14 16:03:42 +0900

    up

--- exercise0509.py
+++ exercise0509.py
@@ -2,19 +2,37 @@ import numpy as np
 from PIL import Image
 import random
 import math
+import cv2
+from matplotlib import pyplot as plt
+
+
+def ReadImage( _img_name ):
+    # read input image
+    img_BGR = cv2.imread(_img_name)
+
+    # convert color BGR to RGB
+    img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+
+    return img_RGB
 
 def main():
+
+    d = 5
+    channel = 3
+    mean = np.empty( (d, channel), float )
+    cov  = np.empty( (channel, channel, d), float )
 
     # 画像読み込み
     for i in range(5):
 
         img                 = np.array(Image.open('image/class' + str(i+1) + '.bmp'))
         height, width       = img.shape[0], img.shape[1]
+
         sum_training_data   = np.array([0]*3)
         sum_training_data2  = np.array([0]*3)
         mean_training_data  = np.array([0]*3)
         mean_training_data2 = np.array([0]*3)
-        
+
         tmp_R = []
         tmp_G = []
         tmp_B = []
@@ -32,7 +50,7 @@ def main():
         mean_training_data  = sum_training_data/10
         mean_training_data2 = sum_training_data2/10
 
-        variance = mean_training_data2 - mean_training_data**2
+        var = mean_training_data2 - mean_training_data**2
 
         sum_RG = 0
         sum_GB = 0
@@ -42,27 +60,61 @@ def main():
 
             sum_RG += (tmp_R[h] - mean_training_data[0])*(tmp_G[h] - mean_training_data[1])
             sum_GB += (tmp_G[h] - mean_training_data[1])*(tmp_B[h] - mean_training_data[2])
-            sum_BR += (tmp_R[h] - mean_training_data[0])*(tmp_B[h] - mean_training_data[2])
+            sum_BR += (tmp_B[h] - mean_training_data[2])*(tmp_R[h] - mean_training_data[0])
 
         cov_RG = sum_RG/10
         cov_GB = sum_GB/10
         cov_BR = sum_BR/10
 
-        cov_Matrix     = np.array([variance[0], cov_RG, cov_BR, cov_RG, variance[1], cov_GB, cov_BR, cov_GB, variance[2]], dtype='float').reshape((3,3))
-        inv_cov_Matrix = np.linalg.inv(cov_Matrix)
-        det_cov_Matrix = np.linalg.det(cov_Matrix)
+        cov_Matrix = np.array([var[0], cov_RG, cov_BR, cov_RG, var[1], cov_GB, cov_BR, cov_GB, var[2]]).reshape((3,3))
+        
+        mean[i, :] = mean_training_data
+        cov[:, :, i] = cov_Matrix
+    # end for
 
-        # print((det_cov_Matrix)**(0.5))
+    print("mean :\n", mean)
+    print("cov :\n", cov)
 
-        D = (np.array([100, 100, 100])-mean_training_data).T*inv_cov_Matrix*(np.array([100, 100, 100])-mean_training_data)
+    satellite_image = ReadImage("./image/irabu_zhang1.bmp")
+    height, width = satellite_image.shape[0], satellite_image.shape[1]
+    result_image = np.empty( (height, width), np.uint8 )
+    for h in range(height):
+        for w in range(width):
+            # Step 3
+            x = satellite_image[h,w,:]
+            # if h == 0 and w == 0:
+            #     print(x)
 
-        # bunbo = ((2*math.pi)**(2.5)) * ((det_cov_Matrix)**(0.5) )
-        # L = np.exp(D) / bunbo
-        # print(L)
-        # print(det_cov_Matrix**(0.5))
-        # test= np.array([100, 100, 100])
-        # print(test.shape)
-        # print(np.array(100, 100, 100)-mean_training_data)
+            # Step 4
+            # Calc likelihood for all class
+            P = []
+            for k in range(d):
+                det = np.linalg.det(cov[:,:,k])
+                inv = np.linalg.inv(cov[:,:,k])
+
+                left = ( 2*np.pi**(d/2) * det**(0.5) ) ** (-1)
+                D = np.dot( np.dot( (x-mean[k,:]).T, inv ), x-mean[k,:] )
+                right = np.exp(-0.5*D)
+                L = left * right
+
+                P.append(L)
+
+            if h == 0 and w == 0:
+                print(np.argmax(P) + 1)
+            result_image[h,w] = np.argmax(P) + 1
+
+            # Show progress
+            if ((h*width+w)+1)%(height*width*0.1) == 0:
+                print((h*width+w)+1, "/", height*width, " pixels done. ")
+    
+    result_image = (result_image / np.max(result_image)) * 255
+    plt.figure(figsize=(8, 6))
+    plt.imshow( result_image, cmap='viridis' )
+    # plt.imshow( result_image, cmap='jet' )
+    plt.colorbar()
+    plt.show()
+
+    np.savetxt("result.txt", result_image, fmt='%d')
 
 if __name__ == "__main__":
     main()
